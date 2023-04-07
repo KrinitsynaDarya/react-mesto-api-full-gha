bbeedded895d00a82e1958e1878cb522750bce6f
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-07 15:01:29 +0300

    fix: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,25 +13,27 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
 
-    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-    // сохраняем список заголовков исходного запроса
-    const requestHeaders = req.headers['access-control-request-headers'];
-
-    // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      // разрешаем кросс-доменные запросы с этими заголовками
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-  }
-      // завершаем обработку запроса и возвращаем результат клиенту
 
 
   }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+}
+    // завершаем обработку запроса и возвращаем результат клиенту
 
    return next();
 };
