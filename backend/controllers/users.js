9b0b4f3a66c177dc66947fb1de5630c57a036d0a
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-09 01:11:06 +0300

    fix: cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
           // token - наш JWT токен, который мы отправляем
           maxAge: 3600 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: 'none', secure: true
         })
         .send({ token });
     })
