93aa021eed58a2867d46bf8d05b886a10d46b16b
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-09 20:38:32 +0300

    fix: cookie

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,33 +1,43 @@
-const router = require('express').Router(); // создали роутер
-const { Joi, celebrate } = require('celebrate');
-const {
-  login,
-  createUser,
-  cookieCheck
-} = require('../controllers/users');
-const { regExUrl } = require('../utils/constants');
+const router = require("express").Router(); // создали роутер
+const { Joi, celebrate } = require("celebrate");
+const { login, createUser, cookieCheck } = require("../controllers/users");
+const { regExUrl } = require("../utils/constants");
 
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(regExUrl),
-  }),
-}), createUser);
-
-router.get('/signout', (req, res) => {
-  res.clearCookie('jwt').send({ message: 'Выход' });
+router.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
 });
 
-router.get('/check', cookieCheck);
+router.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+
+router.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(regExUrl),
+    }),
+  }),
+  createUser
+);
+
+router.get("/signout", (req, res) => {
+  res.clearCookie("jwt").send({ message: "Выход" });
+});
+
+router.get("/check", cookieCheck);
 
 module.exports = router;
