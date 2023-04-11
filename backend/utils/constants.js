d10fb9bd9f5136d64a6f3eff85a1acedf9c6bca7
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-04-11 20:46:56 +0300

    fix: cookie

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -6,7 +6,7 @@ const HTTP_STATUS_NOT_FOUND = 404;
 const HTTP_STATUS_CONFLICT = 409;
 const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
 
-const REGEXP_URL = /https?:\/\/(www.)?[a-z0-9][a-z0-9-.]+\.[a-z]{2,6}[0-9a-z-._~:/?#[\]@!$&'()*+,;=]*#?$/;
+const regExUrl = /https?:\/\/(www.)?[a-z0-9][a-z0-9-.]+\.[a-z]{2,6}[0-9a-z-._~:/?#[\]@!$&'()*+,;=]*#?$/;
 module.exports = {
   HTTP_STATUS_CREATED,
   HTTP_STATUS_BAD_REQUEST,
@@ -15,5 +15,5 @@ module.exports = {
   HTTP_STATUS_NOT_FOUND,
   HTTP_STATUS_CONFLICT,
   HTTP_STATUS_INTERNAL_SERVER_ERROR,
-  REGEXP_URL,
+  regExUrl,
 };
